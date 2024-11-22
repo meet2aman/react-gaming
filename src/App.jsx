158cbb6253cbce4a970c01b82587d84a1c405a08
meet2aman <meet2amankushwaha@gmail.com>
2024-11-22 20:34:52 +0530

    🎉 init a gaming project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="text-orange-500 text-7xl font-black text-center">
+      Hello In the World of Gaming!
+    </div>
+  );
+};
+
+export default App;
